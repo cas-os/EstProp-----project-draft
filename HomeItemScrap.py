7da859807c45eb09d920b34909da3d73b134332c
cas-os <kazimierz.osinski@gmail.com>
2023-08-30 11:07:42 +0200

    3rd commit

--- HomeItemScrap.py
+++ HomeItemScrap.py
@@ -5,8 +5,13 @@ print("====================")
 
 def get_page_links(url):
     r = requests.get(url)
-    sp = BeautifulSoup(r.text, 'lxml')
-    links = sp.select('li.listing-item a')
-    print(links)
+    sp = BeautifulSoup(r.text, 'html.parser')
+    #print("sp #", sp)
+    #links = sp.select('li')
+    links = sp.select('li a')
+    print("Link # ", links)
+    print(sp("product-grid__item"))
 
-get_page_links('http://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/mazowieckie/warszawa/warszawa/warszawa?viewType=listing&limit=72')
+#get_page_links('https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/mazowieckie/warszawa/warszawa/warszawa?viewType=listing&limit=72')
+#get_page_links('https://www.thewhiskyexchange.com/specialoffers')
+get_page_links('https://www.crummy.com/software/BeautifulSoup/bs4/doc/')
