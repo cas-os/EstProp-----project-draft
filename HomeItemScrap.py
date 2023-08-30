08e0d6b5e5fe421a87d975f80dd340115048d133
cas-os <kazimierz.osinski@gmail.com>
2023-08-30 10:10:51 +0200

    2n commit

--- HomeItemScrap.py
+++ HomeItemScrap.py
@@ -1,1 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
 print("====================")
+
+def get_page_links(url):
+    r = requests.get(url)
+    sp = BeautifulSoup(r.text, 'lxml')
+    links = sp.select('li.listing-item a')
+    print(links)
+
+get_page_links('http://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/mazowieckie/warszawa/warszawa/warszawa?viewType=listing&limit=72')
